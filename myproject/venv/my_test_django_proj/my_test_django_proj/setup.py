3631ef7cd997acb9dc53184deec0fd370cdc1d04
Kyle Wilkes <kyle.wilkes@stratagemgroup.com>
2020-06-24 12:29:37 -0600

    working swagger @localhost:8080/swagger

---
+++ myproject/venv/my_test_django_proj/my_test_django_proj/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name ='python_flask_api',
+    version='1.0,'
+    description='REST API using Python',
+    packages=[python_flask_api],
+    install_requires=['Flask']
+)
